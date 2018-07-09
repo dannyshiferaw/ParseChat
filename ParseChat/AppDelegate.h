ed7ee541c03ab07fc921234929286dc3179435ac
Daniel Shiferaw <danielshiferaw@fb.com>
2018-07-09 10:18:49 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Daniel Shiferaw on 7/9/18.
+//  Copyright © 2018 Daniel Shiferaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
